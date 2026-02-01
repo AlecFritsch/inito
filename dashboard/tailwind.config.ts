8208c2da8d61b4ba1569661b788985faabb1498a
FlokzyGOTY <alecsein2acc@gmail.com>
2026-02-02 00:34:24 +0100

    Add Neue Haas Grotesk Display Pro as main font

--- dashboard/tailwind.config.ts
+++ dashboard/tailwind.config.ts
@@ -50,7 +50,7 @@ const config: Config = {
         sm: 'calc(var(--radius) - 4px)',
       },
       fontFamily: {
-        sans: ['var(--font-geist-sans)', 'system-ui', 'sans-serif'],
+        sans: ['Neue Haas Grotesk Display Pro', 'system-ui', 'sans-serif'],
         mono: ['var(--font-geist-mono)', 'monospace'],
       },
     },
